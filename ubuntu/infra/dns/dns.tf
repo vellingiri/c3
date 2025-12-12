71fc7e078591e4a1090c26266384f7cf9ebc3ab0
Vellingiri <vellingiribks@gmail.com>
2025-12-12 12:17:19 +0530

    adding forward and reverse zone for cloud and repo

--- ubuntu/infra/dns/dns.tf
+++ ubuntu/infra/dns/dns.tf
@@ -18,14 +18,32 @@ resource "openstack_dns_zone_v2" "reverse" {
   ttl   = 6000
 }
 
-########################################
-# Static recording for testing
-########################################
-resource "openstack_dns_recordset_v2" "test_record" {
+resource "openstack_dns_recordset_v2" "cloud_f" {
   zone_id = openstack_dns_zone_v2.forward.id
   name    = "cloud.${var.dns_zone_name}"
   type    = "A"
   ttl     = 3000
   records = ["192.168.2.15"]
 }
+resource "openstack_dns_recordset_v2" "cloud_r" {
+  zone_id = openstack_dns_zone_v2.reverse.id
+  name    = "15.${var.reverse_zone_name}"
+  type    = "PTR"
+  ttl     = 3000
+  records = ["cloud.${var.dns_zone_name}"]
+}
 
+resource "openstack_dns_recordset_v2" "repo_f" {
+  zone_id = openstack_dns_zone_v2.forward.id
+  name    = "repo.${var.dns_zone_name}"
+  type    = "A"
+  ttl     = 3000
+  records = ["192.168.2.10"]
+}
+resource "openstack_dns_recordset_v2" "repo_r" {
+  zone_id = openstack_dns_zone_v2.reverse.id
+  name    = "10.${var.reverse_zone_name}"
+  type    = "PTR"
+  ttl     = 3000
+  records = ["repo.${var.dns_zone_name}"]
+}
