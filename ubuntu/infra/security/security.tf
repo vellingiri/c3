c24ddf287489b86a86ff5755202f69b8dcc5baf7
Vellingiri <vellingiribks@gmail.com>
2025-12-12 10:19:04 +0530

    adding k8s security group

--- ubuntu/infra/security/security.tf
+++ ubuntu/infra/security/security.tf
@@ -1,27 +1,8 @@
-###############################################
-# SECURITY GROUP: allow-basic
-###############################################
 resource "openstack_networking_secgroup_v2" "basic" {
-  name        = "allow-basic"
-  description = "Allow SSH + Ping"
+  name        = "k8s"
+  description = "K8s"
 }
 
-###############################################
-# RULE: Allow SSH (TCP/22)
-###############################################
-resource "openstack_networking_secgroup_rule_v2" "allow_ssh" {
-  security_group_id = openstack_networking_secgroup_v2.basic.id
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-}
-
-###############################################
-# RULE: Allow ICMP (Ping)
-###############################################
 resource "openstack_networking_secgroup_rule_v2" "allow_icmp" {
   security_group_id = openstack_networking_secgroup_v2.basic.id
   direction         = "ingress"
@@ -30,3 +11,62 @@ resource "openstack_networking_secgroup_rule_v2" "allow_icmp" {
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "allow_6443" {
+  security_group_id = openstack_networking_secgroup_v2.basic.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "allow_10250" {
+  security_group_id = openstack_networking_secgroup_v2.basic.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 10250
+  port_range_max    = 10250
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "allow_10259" {
+  security_group_id = openstack_networking_secgroup_v2.basic.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 10259
+  port_range_max    = 10259
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "allow_10257" {
+  security_group_id = openstack_networking_secgroup_v2.basic.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 10257
+  port_range_max    = 10257
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "allow_2379_2380" {
+  security_group_id = openstack_networking_secgroup_v2.basic.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 2379
+  port_range_max    = 2380
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "allow_443" {
+  security_group_id = openstack_networking_secgroup_v2.basic.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+}
